f8db2da94b5a043f858bed706ef4e85fcb38d989
milonite <milonite@hotmail.it>
2020-06-09 19:08:49 +0200

    Updates

---
+++ precache-manifest.230ef8fa25cdc52c852206f0d2d5a48d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a19f0e7d3e449010b233b8518ce3a438",
+    "url": "/network2network/index.html"
+  },
+  {
+    "revision": "d009fe4c9daafc1cd244",
+    "url": "/network2network/static/css/main.44c3b5ec.chunk.css"
+  },
+  {
+    "revision": "bc071acc8fef9ee77711",
+    "url": "/network2network/static/js/2.ac8cc142.chunk.js"
+  },
+  {
+    "revision": "1869b20fbcacb9e8628c52cdf1c39001",
+    "url": "/network2network/static/js/2.ac8cc142.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d009fe4c9daafc1cd244",
+    "url": "/network2network/static/js/main.1a314cd3.chunk.js"
+  },
+  {
+    "revision": "98c6ba9ccf92e676e0ff",
+    "url": "/network2network/static/js/runtime-main.e7d30465.js"
+  }
+]);
